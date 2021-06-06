b2ccfbb06760dc3bf4f49ceb90e7b31f95df3c92
Angus <anguscheng389@gmail.com>
2021-06-06 14:20:34 +1000

    Completed leapYears and sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    if (typeof(startNum) != 'number' || typeof(endNum) != 'number') return 'ERROR';
+    if (startNum < 0 || endNum < 0) return 'ERROR';
+    if (isNaN(startNum) || isNaN(endNum)) return 'ERROR';
+    if (startNum > endNum) {
+        let temp = startNum
+        startNum = endNum;
+        endNum = temp;
+    }
+    let result = 0;
+    for (startNum; startNum <= endNum ; startNum++) {
+        result += startNum;
+    }
 
+    return result;
 };
 
 module.exports = sumAll;
