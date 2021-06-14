6844bada2bbcfe72465025692e62a53c1cd82283
Angus <anguscheng389@gmail.com>
2021-06-14 20:57:49 +1000

    Completed Calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(numA = 0, numB = 0) {
+  return numA + numB;
 };
 
-const subtract = function() {
-	
+const subtract = function(numA = 0, numB = 0) {
+  return numA - numB;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  const total = arr.reduce((accumulator, val) => {
+    return accumulator + val;
+  }, 0);
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  const total = arr.reduce((accumulator, currVal) => {
+    return accumulator * currVal;
+  })
 
+  return total;
 };
 
-const power = function() {
-	
+const power = function(val, exp) {
+  return val**exp; 
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+  if (val == 0) return 1;
+  let total = 1;
+  for (let i = 1; i <= val; i++) {
+    total *= i;
+  } 
+
+  return total;
 };
 
 module.exports = {
